6e6ff6ba7953d8d0237e7d7362ac2f9f144dbc83
Misba Qureshi <11misbaqureshi11@gmail.com>
2022-07-21 02:02:31 +0530

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,64 @@
+
+let allMusic = [
+  {
+    name: "Peaches",
+    artist: "Justien Bieber",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Moonlight",
+    artist: "Xtentacion",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Love Nwantiti",
+    artist: "C Kay",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Aglatan Qafe",
+    artist: "Beyjan Beatz",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Closer",
+    artist: "Chainsmoker",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "No Lie",
+    artist: "Dua Lipa",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Bobo",
+    artist: "Aya Nakamura",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Believer",
+    artist: "Imagin Dragons",
+    img: "music-10",
+    src: "music-10"
+  },
+
+];
